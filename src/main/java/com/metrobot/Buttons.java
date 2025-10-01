ff426b4ccb2bd23eec249ec3afde5ea9700a77b6
iamLonelyPlanet <aleksey.nizamov@gmail.com>
2025-10-01 20:25:04 +0500

    Комменты, идеи

--- src/main/java/com/metrobot/Buttons.java
+++ src/main/java/com/metrobot/Buttons.java
@@ -14,7 +14,7 @@ public class Buttons {
     public static final int PAUSE_SHORT_MS = 1200;
     public static final int FIVE_MINUTES_PAUSE_SECONDS = 284;
     public static final int PAUSE_BEFORE_BOSS_MS = 12_800;
-    public static final int PAUSE_TUNNEL_MS = 16_000;
+    public static final int PAUSE_TUNNEL_MS = 16_000; // Для альтернативных скоростей: 16_000, 5_000, 8_000
 
     // --- Максимально допустимое количество боёв. Почему byte? Где ещё их использовать, если не на учёбе! ---
     public static final byte MAX_BATTLES_ARENA = 50;
