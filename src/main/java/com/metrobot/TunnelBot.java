6671d8eca44f06da4135d122cf8ebc063fb075bf
iamLonelyPlanet <aleksey.nizamov@gmail.com>
2026-05-16 20:14:26 +0500

    TunnelBot: вывод отдельных методов для боёв с монстрами. Тестировать в режиме 1 окна (только ящеры)

--- src/main/java/com/metrobot/TunnelBot.java
+++ src/main/java/com/metrobot/TunnelBot.java
@@ -12,11 +12,11 @@ import static com.metrobot.Buttons.*;
 /**
  * Режим "Туннели": ежедневная прогулка перса по туннелям с особыми монстрами: Ящерами и Пауками. Полное прохождение:
  * порядка 45 минут (23 при VIP, 12 при VIP + скоростной одежде).
- * В этом режиме окна не сворачиваются, работа пользователя в Windows крайне нежелательна/почти недопустима.
+ * В этом режиме окна не сворачиваются, работа пользователя в Windows крайне нежелательна.
  * Запись счётчика в файл здесь не нужна.
  * Самый некрасивый класс. Зато самый лёгкий для обучения, для перегрузки, сигнатур, ООП и многого-прочего, что и
  * произошло с апрельской версии 1.2.9
- * TODO: Паузы второго туннеля с пауками. Текст в единый метод. Подумать о паузах по команде пользователя.
+ * TODO: Подумать о паузах по команде пользователя. Тестировать в режиме 1 окна (только ящеры)
  */
 
 public class TunnelBot extends BaseBot {
@@ -51,12 +51,7 @@ public class TunnelBot extends BaseBot {
             showActiveWindows();
             Thread.sleep(PAUSE_BETWEEN_WINDOWS_MS);
 
-//            fightSpiders("Карта ПК-КРО", "Карта КРО-ПК"); // 10 пауков в туннеле Парк Культуры - Кропоткинская
-//            changeLine("Карта ПКк-ПКг", true); // Переход Парк Культуры 1 - Парк Культуры 2, однократно
-//
-//            fightSpiders("Карта ПКг-КИЕ", "Карта КИЕ-ПКг"); // 10 пауков в тоннеле Парк Культуры - Киевская
-//            changeLine("Карта ПКг-ПКк", false); // Переход Парк Культуры Красные - Парк Культуры Ганза, однократно
-//            System.out.printf("\nПауки закончились, прибито %d. Идём к ящерам\n", unifiedCounter.getCount());
+            fightAllSpiders();
 
             Instant endSpiderTime = Instant.now(); // Пока надо для таймера, потом можно удалить
             Duration spidersDuration = Duration.between(startTime, endSpiderTime);
@@ -64,28 +59,13 @@ public class TunnelBot extends BaseBot {
             System.out.printf("На пауков затрачено: %s", printTime(secondsSpider));
 
             // === Туннели с Ящерами ===
-            showActiveWindows(); // можно удалить, но лучше оставить для внутреннего тестирования
+            showActiveWindows();
             unifiedCounter.setCount(0);
             Thread.sleep(PAUSE_SHORT_TUNNELS_MS);
 
-            for (int way = 0; way < MAX_TUNNEL_WAYS; way++) {
-                // 4 ящерицы в тоннелях Парк Культуры - Проспект Вернадского
-                exitStation("Карта-ПК-ФРУ", MonsterKind.LIZARD, PAUSE_SHORT_TUNNELS_MS, true, 0);
-                exitStation("Карта-КОМ", MonsterKind.LIZARD, 0);
-                exitStation("Карта-УНИ", MonsterKind.LIZARD, PAUSE_SHORT_TUNNELS_MS, true, 0);
-                exitStation("Карта-ПВ", MonsterKind.LIZARD, 0);
-                System.out.printf("Завершено пробегов до Проспекта Вернадского: %d\n\n", (way + 1));
+            fightAllLizards();
 
-                // 4 ящерицы в тоннелях Проспект Вернадского - Парк Культуры
-                exitStation("Карта-УНИ", MonsterKind.LIZARD, PAUSE_SHORT_MS);
-                exitStation("Карта-КОМ", MonsterKind.LIZARD, PAUSE_SHORT_MS, true, PAUSE_SHORT_MS);
-                exitStation("Карта-КОМ-ФРУ", MonsterKind.LIZARD, 0);
-                exitStation("Карта-ФРУ-ПК", MonsterKind.LIZARD, PAUSE_SHORT_MS, false, PAUSE_SHORT_MS);
-                System.out.printf("\nЗавершено пробегов до Парка Культуры: %d\n\n", (way + 1));
-            }
-
-            // Пока надо для таймера, потом можно удалить
-            Duration lizardDuration = Duration.between(endSpiderTime, Instant.now());
+            Duration lizardDuration = Duration.between(endSpiderTime, Instant.now()); // Для таймера, потом удалить
             long secondsLizard = lizardDuration.getSeconds();
             System.out.printf("На ящеров затрачено: %s", printTime(secondsLizard));
             System.out.printf("Итого на режим %s затрачено %s", botName, printTime(secondsSpider + secondsLizard));
@@ -170,8 +150,34 @@ public class TunnelBot extends BaseBot {
         }
     }
 
-    // Подсчёт и формирование строки с потраченным временем
-    private String printTime(long seconds) {
-        return String.format("%d мин %d сек\n", seconds / 60, seconds % 60);
+    private void fightAllSpiders() throws InterruptedException {
+        fightSpiders("Карта ПК-КРО", "Карта КРО-ПК"); // 10 пауков в туннеле Парк Культуры - Кропоткинская
+        changeLine("Карта ПКк-ПКг", true); // Переход Парк Культуры 1 - Парк Культуры 2, однократно
+        fightSpiders("Карта ПКг-КИЕ", "Карта КИЕ-ПКг"); // 10 пауков в тоннеле Парк Культуры - Киевская
+        changeLine("Карта ПКг-ПКк", false); // Переход Парк Культуры Красные - Парк Культуры Ганза, однократно
+        System.out.printf("\nПауки закончились, прибито %d. Идём к ящерам\n", unifiedCounter.getCount());
     }
-}
+
+    private void fightAllLizards() throws InterruptedException {
+        for (int way = 0; way < MAX_TUNNEL_WAYS; way++) {
+            // 4 ящерицы в тоннелях Парк Культуры - Проспект Вернадского
+            exitStation("Карта-ПК-ФРУ", MonsterKind.LIZARD, PAUSE_SHORT_TUNNELS_MS, true, 0);
+            exitStation("Карта-КОМ", MonsterKind.LIZARD, 0);
+            exitStation("Карта-УНИ", MonsterKind.LIZARD, PAUSE_SHORT_TUNNELS_MS, true, 0);
+            exitStation("Карта-ПВ", MonsterKind.LIZARD, 0);
+            System.out.printf("Завершено пробегов до Проспекта Вернадского: %d\n\n", (way + 1));
+
+            // 4 ящерицы в тоннелях Проспект Вернадского - Парк Культуры
+            exitStation("Карта-УНИ", MonsterKind.LIZARD, PAUSE_SHORT_MS);
+            exitStation("Карта-КОМ", MonsterKind.LIZARD, PAUSE_SHORT_MS, true, PAUSE_SHORT_MS);
+            exitStation("Карта-КОМ-ФРУ", MonsterKind.LIZARD, 0);
+            exitStation("Карта-ФРУ-ПК", MonsterKind.LIZARD, PAUSE_SHORT_MS, false, PAUSE_SHORT_MS);
+            System.out.printf("\nЗавершено пробегов до Парка Культуры: %d\n\n", (way + 1));
+        }
+    }
+
+        // Подсчёт и формирование строки с потраченным временем
+        private String printTime ( long seconds){
+            return String.format("%d мин %d сек\n", seconds / 60, seconds % 60);
+        }
+    }
